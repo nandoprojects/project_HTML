f600aa641ca4f1fca4395ac212b62054fb88f7e4
user11 <fernandobernardodacruz@gmail.com>
2025-08-08 22:48:48 -0300

    Add files via upload

---
+++ GitHubProjeto/app/src/main/java/com/example/myapplication/logica.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class logica extends AppCompatActivity {
+
+
+    Button botão_logar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.tela_design);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        botão_logar = findViewById(R.id.btn_avancar);
+
+        botão_logar
+    }
+}
